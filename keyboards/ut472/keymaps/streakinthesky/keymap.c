631c7e014ad210c5d02999819af02271d1bbe5f5
Ross Baquir <streakinthesky@gmail.com>
2019-07-03 03:14:42 -0700

    Fix typo with LGUI

--- keyboards/ut472/keymaps/streakinthesky/keymap.c
+++ keyboards/ut472/keymaps/streakinthesky/keymap.c
@@ -40,7 +40,7 @@ LAYOUT(
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, L4_QUOT,
   TB_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-  KC_CAPS, MO(3),   KC_LALT, KC_GUI,  MO(2),      KC_SPC,        MO(1),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_CAPS, MO(3),   KC_LALT, KC_LGUI,  MO(2),      KC_SPC,        MO(1),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
   /* FN Layer 1
